d94ec5a35444d15e47d6a831db39936cfa3b6a9c
Kayaroganam V <88090401+Kayaroganam@users.noreply.github.com>
2023-05-08 19:27:15 +0530

    Update Install_dc_ubuntu.sh

--- Install_dc_ubuntu.sh
+++ Install_dc_ubuntu.sh
@@ -2,5 +2,5 @@ wget http://kaya-16339/linux_falcon-sensor_6.45.0-14203_amd64.deb ./
 sudo dpkg -i linux_falcon-sensor_6.45.0-14203_amd64.deb
 sudo /opt/CrowdStrike/falconctl -s --cid=F4BF9189CB9C4CA48982E367D2F819CC-D8
 sudo systemctl restart falcon-sensor
-sleep 5;
+sleep 5
 sudo systemctl status falcon-sensor
